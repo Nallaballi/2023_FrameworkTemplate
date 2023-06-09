bfe55bbe793c7ab3d9f0d75e8f3afceae1651822
Sanjay Nallaballi <fsginfodesk@gmail.com>
2023-06-08 20:06:58 -0500

    June changes

---
+++ src/test/java/env/qa/TestCases/NewTest.java
@@ -0,0 +1,13 @@
+package env.qa.TestCases;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  
+	
+	@Test
+  public void firstTest()
+  {
+	  System.out.println("Print first test");
+  }
+}
